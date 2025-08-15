76ac7839caeaadb1dd2455d477474c710680f4e8
MuhamadSyahrudin <muhamadsyahrudin2309@gmail.com>
2025-08-15 09:35:45 +0700

    model keras

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ if not os.path.exists(MODEL_PATH):
     print("Downloading model from Google Drive...")
     gdown.download(MODEL_URL, MODEL_PATH, quiet=False)
 
-model = load_model(MODEL_PATH, compile=False)
+model = keras.models.load_model(MODEL_PATH, compile=False)
 
 class_names = ['Bukan Cabai', 'Kering', 'Matang', 'Mentah', 'Setengah Matang']
 
